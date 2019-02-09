0e886955961f18652f3ed3b9db2dc17a6d441185
Ben Walker <bwalke05@gmail.com>
2019-02-09 14:44:52 -0500

    Add start and end point to ray struct, function to retrieve rays, expose max ray count

--- include/raygun.h
+++ include/raygun.h
@@ -1,13 +1,22 @@
 #ifndef RAYGUN_
 #define RAYGUN_
 
+#include "point.h"
+#include <stdbool.h>
+
+enum RAY_COUNT { RAY_COUNT = 10 };
+
 typedef struct Ray {
    int id;
    long spawnTime;
+   bool active;
+   Point start, end;
 } Ray;
 
 void fireRay();
 
 void fizzleRays();
+
+Ray *getRays();
 
 #endif // RAYGUN_
