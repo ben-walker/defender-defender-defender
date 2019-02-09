bd90c7addef91e477f8a23686d3869ca3fbb56db
Ben Walker <bwalke05@gmail.com>
2019-02-09 14:46:37 -0500

    Use external Point lib

--- include/humanHerd.h
+++ include/humanHerd.h
@@ -1,15 +1,10 @@
 #ifndef HUMAN_HERD_
 #define HUMAN_HERD_
 
+#include "point.h"
+
 enum MAX_HUMANS { MAX_HUMANS = 10 };
 enum NAME_LEN { NAME_LEN = 25 };
-
-typedef struct Point {
-   int x;
-   int y;
-   int z;
-   int color;
-} Point;
 
 typedef struct Human {
    Point head;
